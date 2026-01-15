1512e89ca5dc5dc05d5b1ccde3f2be805dd9e81d
Kadem <kadem.garnier@gmail.com>
2026-01-15 11:58:33 +0100

    port

---
+++ ecosystem.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  apps: [
+    {
+      name: 'guess-player-frontend',
+      script: 'npm',
+      args: 'start',
+      cwd: './',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3002  // Change ce port si nécessaire
+      },
+      error_file: './logs/frontend-error.log',
+      out_file: './logs/frontend-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      restart_delay: 4000
+    },
+    {
+      name: 'guess-player-backend',
+      script: 'server.js',
+      cwd: './backend',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        SOCKET_PORT: 3003  // Change ce port si nécessaire
+      },
+      error_file: '../logs/backend-error.log',
+      out_file: '../logs/backend-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      restart_delay: 4000
+    }
+  ]
+};
